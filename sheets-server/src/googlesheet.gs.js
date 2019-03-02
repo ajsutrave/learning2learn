153948ddb112bca859a5ccb4a7664449b9a63d5c
AJ Sutrave <abhijeetsutrave@gmail.com>
2019-03-02 00:38:41 -0600

    Change from GET to POST

--- sheets-server/src/googlesheet.gs.js
+++ sheets-server/src/googlesheet.gs.js
@@ -1,7 +1,7 @@
 // original from: http://mashe.hawksey.info/2014/07/google-sheets-as-a-database-insert-with-apps-script-using-postget-methods-with-ajax-example/
 // original gist: https://gist.github.com/willpatera/ee41ae374d3c9839c2d6
 
-function doGet(e){
+function doPost(e){
   return handleResponse(e);
 }
 
