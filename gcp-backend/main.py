63c1fef29101355cfab70b8c6fcb6b0368028e68
AJ Sutrave <abhijeetsutrave@gmail.com>
2019-03-02 00:07:38 -0600

    rename

--- gcp-backend/main.py
+++ gcp-backend/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, url_for
+from flask import request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def api_root():
-    return jsonify({})
+    print request.get_json()
+    return jsonify({"test":"123"})
 
 if __name__ == '__main__':
     app.run()
